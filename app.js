8b12feb8c87248184dd1277daae9e0623c283fab
mufaiz <mufaiz@mufaizs-MacBook-Pro.local>
2026-03-01 23:35:39 +0300

    Employee details page implemented

--- app.js
+++ app.js
@@ -14,6 +14,18 @@ app.get("/", async (req, res) => {
   const employees = await business.listEmployees();
   res.render("landing", { employees });
 });
+// Employe details 
+app.get("/employee/:id", async (req, res) => {
+  const empId = req.params.id;
+
+  const employee = await business.getEmployee(empId);
+
+  if (!employee) {
+    return res.send("Employee not found");
+  }
+
+  res.render("employee", { employee });
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
